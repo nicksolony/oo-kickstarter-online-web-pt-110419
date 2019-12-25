a51bbe7dc96b9d42d889fbcc093e4c8135c1a3af
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 15:53:40 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,8 +3,8 @@ class Project
   #attr_accessor :backed_projects
   
   
-  def initialize (name)
-    @name = name
+  def initialize (title)
+    @title = title
    # @backed_projects = []
   end
 end
